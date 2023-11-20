ae6defb170cd6007a6f82ca24ae72f78f06f65b3
Adimachukwu Benjamin <thegreatben27@gmail.com>
2023-11-20 17:12:27 +0100

    finished the Authentication

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter>
+   <QueryProvider>
+   <AuthProvider>
+      <App />
+    </AuthProvider>
+   </QueryProvider>
+  </BrowserRouter>
+);
